ac33be188cb1f9e1f5bbdb2a9665dea7a05000d5
Manuel Pineyro <manuel.o.pineyro@gmail.com>
2015-09-01 00:33:01 -0700

    First test for get request

--- spec/apis/coupons_api_spec.rb
+++ spec/apis/coupons_api_spec.rb
@@ -7,9 +7,15 @@ end
 describe CouponsApi do
   include Rack::Test::Methods
 
-  describe 'e.g. GET, POST, PUT, etc.' do
-    it 'needs tests to be written!' do
-      pending('write tests for CouponsApi!')
+  before(:each) do
+    @coupon = create(:coupon)
+  end
+
+  describe 'GET /coupons' do
+    it 'Should return an array of coupons' do
+      get '/coupons'
+
+      expect(response_body).to eq({"data": [{ object_type: 'coupon', id: '#{@coupon.id}', name: '#{@coupon.name}', points: '#{@coupon.points}', number_available: '#{@coupon.number_available}' }]}.to_json)
     end
   end
 
